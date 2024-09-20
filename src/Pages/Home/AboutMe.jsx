0278ed5e6ed2107afdede89f284a168a77cfec48
mohsinali shaikh <mohsinalishaikh987@gmail.com>
2024-09-20 16:47:46 +0530

    new details added

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -8,13 +8,13 @@ export default function AboutMe(){
         <div className="hero--section--content--box about--section--box">
             <p className="hero--section--box">About</p>
             <h1 className="skills--section--heading">About Me</h1>
-            <p className="hero--section--description">
+            <p className="hero--section--description about-description ">
             I started my web development journey with basic technologies like HTML, CSS, JS and later move to learn js library like React.js.
             </p>
-            <p className="hero--section--description">
+            <p className="hero--section--description about-description ">
             Then I move on to joint Mak Byte as Frontend Intern for 9 months which eventually became a core base for my career which will lead to my future goals.
             </p>
-            <p className="hero--section--description">
+            <p className="hero--section--description about-description ">
             Along with my 9 months of Internship experience and projects which I have mentioned below I also have a certificate from udemy for completion of frontend language course.
             </p>
         </div>
--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -3,7 +3,7 @@
 export default function HeroSection (){
     return (
         <section id="heroSection" className="hero--section">
-        <div className="hero--section--content--box">
+        <div className="hero--section--content--box content-box">
         <div className="hero--section--content">
             <p className="section--title">Hey, I am Mohsinali</p>
                 <h1 className="hero--section--title">
@@ -11,7 +11,7 @@ export default function HeroSection (){
                     <br/>
                 </h1>     
                 <p className="hero--section--description">
-                I am an enthusiastic and motivated web developer with hands-on experience of 6-months at Mak byte in HTML, CSS, JavaScript, and React.js for creating responsive and engaging user interfaces.
+                I am an enthusiastic and motivated web developer with hands-on experience of 9-months at Mak byte in HTML, CSS, JavaScript, and React.js for creating responsive and engaging user interfaces.
                 <br/>
                 <br/>
                     I am currently open to work and ready to join.
